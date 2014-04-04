dd9f1ca4008ab7a69ccda52097d0165967b85832
Geoff Webb <geoff@unbounce.com>
2014-04-04 11:05:39 -0700

    WIP: profiles and raw dotfiles

---
+++ profile.d/rbenv.sh
@@ -0,0 +1,3 @@
+if [ -f "/usr/local/bin/rbenv" ] ; then
+  eval "$(rbenv init -)"
+fi
---
+++ profile.d/vertx.sh
@@ -0,0 +1,4 @@
+if [ -d "/opt/vert.x" ] ; then
+  export VERTX_HOME=/opt/vert.x
+  export PATH=$VERTX_HOME/bin:$PATH
+fi
