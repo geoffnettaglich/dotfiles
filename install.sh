4d3445692e0b9d7d7707867b51713b24e3623b46
Geoff Webb <geoff@unbounce.com>
2015-04-09 22:50:56 -0700

    Simple instal script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+set -x
+
+DIR=$(dirname $0)
+RAW="$DIR/raw/*"
+FILES=`find $DIR/raw/* -type f`
+
+for FILE in $RAW ; do
+  NAME=$(basename "$FILE")
+  if [ -n "$NAME" ] ; then
+    FILEPATH="$HOME/.$NAME"
+    if [ -r $FILEPATH ] ; then
+      echo "File exists $FILEPATH"
+    else
+      echo "Creating file $FILEPATH"
+      cp $RAW $FILEPATH
+    fi
+  fi
+done
