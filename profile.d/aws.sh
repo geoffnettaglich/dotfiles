d03a69c4da77d2b28dda5c5a8a5dd2bf951a3737
Geoff Webb <geoff@unbounce.com>
2018-08-21 10:07:59 -0700

    Add ability to automate auditing tags across regions

--- profile.d/aws.sh
+++ profile.d/aws.sh
@@ -13,35 +13,9 @@ if [ -d $DIR ] ; then
   fi
 fi
 
-function aws-lookup() {
-  aws ec2 describe-instances \
-          --region $1 \
-          --filters "Name=tag:Name,Values=*$2*" \
-          --query='Reservations[*].Instances[*].[PublicDnsName,Tags[*].Value]' \
-          --output text
-}
-
-function aws-filter-args ()
-{
-    NAME="";
-    STATE="*";
-    for i in "$@";
-    do
-        echo "I is $i";
-        case $i in
-            -n=* | --name=*)
-                NAME="${i#*=}"
-            ;;
-            -s=* | --state=*)
-                STATE="${i#*=}"
-            ;;
-        esac;
-    done
-}
-
 function aws-query-tags ()
 {
-    echo "[Tags[?Key==\`Name\`].Value] [0][0], [Tags[?Key==\`project\`].Value] [0][0], [Tags[?Key==\`environment\`].Value] [0][0], [Tags[?Key==\`owner\`].Value] [0][0]"
+    echo "[Tags[?Key==\`Name\`].Value] [0][0], [Tags[?Key==\`project\`].Value] [0][0], [Tags[?Key==\`environment\`].Value] [0][0], [Tags[?Key==\`owner\`].Value] [0][0], [Tags[?Key==\`version\`].Value] [0][0]"
 }
 
 function aws-query-tags-names ()
@@ -51,32 +25,54 @@ function aws-query-tags-names ()
 
 function aws-instances ()
 {
-    aws-filter-args $@;
+    local NAME=${1:-*} STATE=${2:-*};
     echo "Using: name=$NAME and state=$STATE" 1>&2;
-    echo -n "InstanceId\tInstanceType\tImageId\tState.Name\tLaunchTime\tPlacement.AvailabilityZone\tPlacement.Tenancy\tPrivateIpAddress\tPrivateDnsName\tPublicDnsName"
+    echo -e -n "InstanceId\tInstanceType\tImageId\tState.Name\tLaunchTime\tPlacement.AvailabilityZone\tPrivateIpAddress\tPrivateDnsName\tPublicDnsName\t"
     aws-query-tags-names;
     aws ec2 describe-instances \
             --filters "Name=instance-state-name,Values=$STATE" "Name=tag:Name,Values=*$NAME*" \
-            --query "Reservations[*].Instances[*].[InstanceId, InstanceType, ImageId, State.Name, LaunchTime, Placement.AvailabilityZone, Placement.Tenancy, PrivateIpAddress, PrivateDnsName, PublicDnsName, `aws-query-tags`]" \
+            --query "Reservations[*].Instances[*].[InstanceId, InstanceType, ImageId, State.Name, LaunchTime, Placement.AvailabilityZone, PrivateIpAddress, PrivateDnsName, PublicDnsName, `aws-query-tags`]" \
             --output text
 }
 
 function aws-lookup ()
 {
+    local REGION=${1:-*} NAME=${2:-*};
     aws ec2 describe-instances \
-            --region $1 \
-            --filters "Name=tag:Name,Values=*$2*" \
+            --region $REGION \
+            --filters "Name=tag:Name,Values=*$NAME*" \
             --query='Reservations[*].Instances[*].[PublicDnsName,Tags[*].Value]' \
             --output text
 }
 
 function aws-security-groups ()
 {
-    local NAME=${1:-*} STATE=${2:-*};
+    local NAME=${1:-*}
     echo "Using: name=$NAME and Region=$AWS_DEFAULT_REGION" 1>&2;
     echo -e "GroupId\tGroupName\tDescription\t`aws-query-tags-names`";
     aws ec2 describe-security-groups \
             --filters "Name=group-name,Values=*$NAME*" \
             --query "SecurityGroups[*].[GroupId, GroupName, Desciption, `aws-query-tags`]" \
             --output text
+}
+
+function aws-instance-counts ()
+{
+    for region in $(aws ec2 describe-regions --query 'Regions[].RegionName' --output text); do 
+        echo "Region: $region"; aws ec2 describe-instances --region "$region" --query 'Reservations[].Instances[] | length(@)'; 
+    done
+}
+
+function aws-instance-tags ()
+{
+  REGIONS="ap-southeast-1 eu-central-1 eu-west-1 us-east-1 us-west-1 us-west-2"
+
+  for REGION in $REGIONS ; do
+    LINES=`AWS_DEFAULT_REGION=$REGION aws-instances`
+
+    while read -r line; do
+      echo "$REGION $line"
+    done <<< "$LINES"
+
+  done
 }
